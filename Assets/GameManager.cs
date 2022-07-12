d40bb38716868fa8fe66a31d2e9f35734d196822
hraka <laqahraca@protonmail.com>
2022-07-12 18:54:19 +0900

    220712 이동, 들어올리기
    특정 영역 위에서만 이동하게 할 수는 없나?

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -13,45 +13,65 @@ public class GameManager : MonoBehaviour
     public bool triger = true;
     public int mapPositiveIndex;
     public int mapNegativeIndex;
+    public int itemPerGround;
     // Start is called before the first frame update
     void Start()
     {
         manager = this;
 
-
-        for(int i = 0; i < 0; i++)
+        for (int i = 0; i < 0; i++)
         {
             var instance = Instantiate(ground, new Vector3((i + 1) * ground.transform.localScale.x, -5, 0), Quaternion.identity, earth);
             if (i % 2 == 0)
                 instance.GetComponent<SpriteRenderer>().color = Color.green;
         }
 
-        for (int i = 0; i < 5; i++)
+        Instantiate(ground, new Vector3(0, -5, 0), Quaternion.identity, earth);
+        /*for (int i = 0; i < 5; i++)
         {
             var instance = Instantiate(item, new Vector3(Random.Range(0f, 30f), Random.Range(-3f, 0f), 0), Quaternion.identity);
             if (i % 2 == 0)
                 instance.GetComponent<SpriteRenderer>().color = Color.gray;
             instance.name = "Item";
-        }
+        }*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.transform.position.x > mapPositiveIndex * 5)
-        { 
-            var instance = Instantiate(ground, new Vector3((mapPositiveIndex + 1) * ground.transform.localScale.x, -5, 0), Quaternion.identity, earth);
+        InstantiateMap();
+
+    }
+
+    public void InstantiateMap()
+    {
+        if (player.transform.position.x > mapPositiveIndex * 10)
+        {
+            var instance = Instantiate(ground, new Vector3((mapPositiveIndex + 1) * 10, -5, 0), Quaternion.identity, earth);
             mapPositiveIndex += 1;
             if (mapPositiveIndex % 2 == 0)
                 instance.GetComponent<SpriteRenderer>().color = Color.green;
+
+
+            for (int i = 0; i < itemPerGround; i++)
+            {
+                var itemInstance = Instantiate(item, new Vector3(Random.Range(mapPositiveIndex * 10 - 5f, mapPositiveIndex * 10 + 5f), Random.Range(-3f, 0f), 0), Quaternion.identity);
+                itemInstance.name = "Item";
+            }
+            
         }
-        if (player.transform.position.x < mapNegativeIndex * -5)
+        if (player.transform.position.x < (mapNegativeIndex) * -10)
         {
-            var instance = Instantiate(ground, new Vector3((mapNegativeIndex + 1) * ground.transform.localScale.x * -1, -5, 0), Quaternion.identity, earth);
+            var instance = Instantiate(ground, new Vector3((mapNegativeIndex + 1) * 10 * -1, -5, 0), Quaternion.identity, earth);
             mapNegativeIndex += 1;
             if (mapNegativeIndex % 2 == 0)
                 instance.GetComponent<SpriteRenderer>().color = Color.green;
-        }
 
+            for (int i = 0; i < itemPerGround; i++)
+            {
+                var itemInstance = Instantiate(item, new Vector3(Random.Range(mapNegativeIndex * -10 - 5f, mapNegativeIndex * -10 + 5f), Random.Range(-3f, 0f), 0), Quaternion.identity);
+                itemInstance.name = "Item";
+            }
+        }
     }
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -18,7 +18,12 @@ public class Player : MonoBehaviour
     void Update()
     {
 
-        transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0) * Time.deltaTime * speed;
+        var velocity = Vector2.zero;
+        velocity.x = Input.GetAxisRaw("Horizontal") * speed;
+        velocity.y = Input.GetAxisRaw("Vertical") * speed;
+        GetComponent<Rigidbody2D>().velocity = velocity; //벽에 부딪히면 0이 우선으로 들어간다?
+                                                         //transform.position += new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0) * Time.deltaTime * speed;
+
 
 
         if (Input.GetKeyDown(KeyCode.F) && isPicking)
@@ -39,5 +44,7 @@ public class Player : MonoBehaviour
             collision.gameObject.transform.localPosition = getPoint.transform.localPosition;
             isPicking = true;
         }
+       
     }
+   
 }
